452d2c20a28f3144ca70ad8d81c6f1af950ead5e
unknown <mchughaw@gmail.com>
2015-05-14 22:32:45 -0700

    initial commit

---
+++ ChatroomClient.py
@@ -0,0 +1,291 @@
+from PodSixNet.Connection import connection
+from PodSixNet.Connection import ConnectionListener
+import thread
+from time import sleep
+import sys
+
+silence = []
+admin = False
+firstTime = True
+
+class MyNetworkListener(ConnectionListener):
+
+    def Network(self, data):
+        if not data["action"] in ignoreActions:
+            print 'network data:', data
+        pass
+
+    def Network_connected(self, data):
+        print "connected to the server"
+
+    def Network_error(self, data):
+        print "error:", data['error'][1]
+        sleep(2)
+        sys.exit()
+
+    def Network_disconnected(self, data):
+        print "disconnected from the server"
+        sleep(2)
+        sys.exit()
+        
+    def Network_post(self, data):
+        u = data["username"].lower()
+        if not u in silence:
+            print data["username"] + " : " + data["message"]
+        
+    def Network_hug(self, data):
+        print "You have been hugged by " + data["from"]
+        
+    def Network_userList(self, data):
+        global isListing
+        global userList
+        global firstTime
+        tempUsers = data["message"]
+        if isListing:
+            if len(tempUsers) == 1:
+                print "There is currently one person on this server:"
+            else:
+                print "There are currently " + str(len(tempUsers)) + " people on this server:" 
+            for i in tempUsers:
+                print i
+            isListing = False
+        else:
+            userList = data["message"]
+        if firstTime:
+            firstTime = False
+            
+               
+            
+    def Network_adminAccept(self, data):
+        global admin
+        admin = True
+        
+    def Network_newUsername(self, data):
+        global username
+        print "New username : " + data["username"]
+        username = data["username"]
+        
+        
+        
+        
+def hug(user):
+    print "Hugging user " + user
+    networkListener.Send({"action":"hug","to":user,"from":username})
+    
+def silenceUser(user):
+    silence.append(user.lower())
+    print silence
+    print "User " + user + " has been silenced"
+
+def unsilenceUser(user):
+    u = user.lower()
+    global silence
+    if u in silence:
+        index = silence.index(user.lower())
+        silence = silence[:index] + silence[index+1:]
+    else:
+        print "HELP : " + user + " is not silenced"
+    
+def list():
+    global isListing
+    networkListener.Send({"action":"getusers","username":username})
+    isListing = True
+    
+def askAdminPassword(password):
+    networkListener.Send({"action":"passwordRequest","username":username,"message":password})
+    
+def newUsername(newUsername):
+    global username
+    if usernameCheck(newUsername):
+        networkListener.Send({"action":"changeUsername","newUsername":newUsername,"oldUsername":username})
+    else:
+        print "HELP : " + newUsername + " is an invalid username."
+    
+def commandManager(input):
+    global commands
+    global commandNames
+    if input[0] == "/":
+        # These commands don't take any arguments
+        
+        if input == "/exit":
+            sys.exit()
+        elif input == "/list":
+            list()
+        elif input == "/n":
+            print
+            
+        else: #Split the word to get the args
+            words = input.split(" ")
+            """
+            word = ""
+            for i in input:
+                if i == " ":
+                    words.append(word)
+                    word = ""
+                else:
+                    word = word + i
+            words.append(word)
+            """
+            # All these commands take in 1+ arguments
+            
+            if words[0] == "/hug":
+                if len(words) >= 2:
+                    hug(words[1])
+                else:
+                    print "HELP : /hug requires two arguments. You had " + str(len(words)) + " argument(s)."
+            elif words[0] == "/silence":
+                if len(words) >= 2:
+                    silenceUser(words[1])
+                else:
+                    print "HELP : /silence requires two arguments. You had " + str(len(words)) + " argument(s)."
+            elif words[0] == "/desilence":
+                if len(words) >= 2:
+                    unsilenceUser(words[1])
+                else:
+                    print "HELP : /desilence requires two arguments. You had " + str(len(words)) + " argument(s)."
+                    
+            elif words[0] == "/username":
+                if admin:
+                    if len(words) == 2:
+                        newUsername(words[1])
+                    else:
+                        print "HELP : /username requires two arguments. You had " + str(len(words)) + " argument(s)."
+                else:
+                    print "HELP : You do not have permission to that command."
+                    
+            elif words[0] == "/help":
+                if len(words) == 2:
+                    print "HELP : " + commands[str(words[1])]
+                else:
+                    print "There are " + str(len(commandNames)) + " commands currently:"
+                    for i in commandNames:
+                        print "/" + i + " : " + commands[i]
+                    
+            elif words[0] == "/admin":
+                if len(words) >= 2:
+                    if words[1] == "join":
+                        if len(words) >= 3:
+                            askAdminPassword(words[2])
+                        else:
+                            print "HELP : /admin join requires a password."
+                    elif words[1] == "give":
+                        if len(words) >= 3:
+                            #giveAdmin(words[3])
+                            pass
+                        else:
+                            print "HELP : /admin give requires a username to give admin status"
+                else:
+                    print commands["admin"]
+                        
+            else:
+                print "HELP : Command " + input + " not found. type /help for more help"
+                
+        return False
+    else:
+        return True
+        
+def stopRunning():
+    run = False
+        
+def inputer(input):
+    tempInput = raw_input()
+    input.append(tempInput)
+        
+def usernameCheck(testingUsername):
+    global userList
+    if not len(testingUsername.split(" ")) == 1:
+        return False
+    if testingUsername in userList:
+        return False
+    return True
+        
+def loop():
+    input = []
+    i = 0
+    thread.start_new_thread(inputer, (input,))
+    while run:
+        networkListener.Pump()
+        connection.Pump()
+        if input:
+            message = input[0]
+            canProceed = commandManager(message)
+            if canProceed:
+                networkListener.Send({"action":"post","username":username,"message":str(message)})
+                input = []
+                message = None
+            loop()
+        if i == 10:
+            networkListener.Send({"action":"getusers","username":username})
+            i = 0
+        else:
+            i += 1
+        sleep(0.05)
+        
+#Start of the program
+userList = []
+ignoreActions = ["post","hug","userList","connected","adminAccept","newUsername"]
+isListing = False
+usernameTest = False
+
+commands = {
+"silence":"Usage: /silence [username] - Used if you want to block people's messages",
+"desilence":"Usage: /desilence [username] - Used if you want to unblock people's messages",
+"hug":"Usage: /hug [username] - Used to hug people :)",
+"admin":"Usage: /admin [join, give] ... - Used to give or get admin permission",
+"username":"Usage: [ADMIN ONLY] /username [New Username] - Used to change your username",
+"help":"Usage: /help OR /help [command] - Used if you want to find out more about something",
+"/n":"Usage: /n - Used to make a new line",
+"exit":"Usage: /exit - Used to exit the chat",
+"list":"Usage: /list - Used to list everyone in the chat"
+}
+
+commandNames = ["silence","desilence","hug","admin","username","help","/n","exit","list"]
+
+address=raw_input("Address of Server (192.168.1.138:8000): ")
+if not address:
+    host, port="67.161.127.21", 8000
+else:
+    host, port=address.split(":")
+        
+print "Connecting to server on host: %s, port:%s" % (host, int(port))
+networkListener = MyNetworkListener()
+try:
+  networkListener.Connect((host, int(port)))
+except:
+  print "Error connecting to server"
+  sleep(3)
+  exit()
+  
+networkListener.Send({"action":"firstGetUsers","message":"Hi"})
+
+count = 0
+
+"""
+while userList == []:
+    sleep(0.1)
+    count += 1
+    print count
+"""
+    
+print userList
+  
+username = " "
+
+while firstTime:
+    networkListener.Pump()
+    connection.Pump()
+    sleep(0.05)
+
+while username == " ": 
+    tempUsername = raw_input("What is your username: ")
+    if usernameCheck(tempUsername):
+        username = tempUsername
+    else:
+        print "Please enter a valid username."
+
+networkListener.Send({"action":"username","message":str(username)})
+run = True
+sleep(2)
+print "Starting loop"
+loop()
+raw_input("")
---
+++ ChatroomServer.py
@@ -0,0 +1,136 @@
+from PodSixNet.Connection import connection
+import PodSixNet.Channel
+import PodSixNet.Server
+from time import sleep
+class ClientChannel(PodSixNet.Channel.Channel):
+
+    def Network(self, data):
+        #print data["message"]
+        pass
+        
+    def Network_username(self, data):
+        print "Creating User!"
+        boxesServe.postToAll("SERVER", "User " + data["message"] + " has joined the server.")
+        boxesServe.createUser(data["message"])
+            
+    def Network_post(self, data):
+        tempUsername = data["username"]
+        tempMessage = data["message"]
+        boxesServe.postToAll(tempUsername, tempMessage)
+        
+    def Network_hug(self, data):
+        boxesServe.slashHug(data["to"], data["from"])
+        
+    def Network_getusers(self, data):
+        boxesServe.sendUsernames(data["username"])
+        
+    def Network_passwordRequest(self, data):
+        boxesServe.passwordRequest(data["username"], data["message"])
+        
+    def Network_changeUsername(self, data):
+        boxesServe.changeUsername(data["oldUsername"], data["newUsername"])
+        
+    def Network_firstGetUsers(self, data):
+        boxesServe.firstSendUsernames()
+        
+class User():
+    def __init__(self, channel, addr, username):
+        self.channel = channel
+        self.address = addr
+        self.username = username
+    def getChannel(self):
+        return self.channel
+    def getUsername(self):
+        return self.username
+    def setUsername(self, newUsername):  
+        self.username = newUsername
+
+class BoxesServer(PodSixNet.Server.Server):
+ 
+    channelClass = ClientChannel
+ 
+    def Connected(self, channel, addr):
+        self.channel1 = channel
+        self.tempChannel = channel
+        self.tempAddr = addr
+        print self.tempChannel
+        print 'new connection:', channel
+        
+    def sendCorrect(self):
+        self.channel1.Send({"action":"message","message":"You are correct"})
+        print "'You are correct' sent!"
+        
+    def createUser(self, username):
+        user = User(self.tempChannel, self.tempAddr, username)
+        users.append(user)
+        
+    def sendToAll(self, message):
+        for i in users:
+            channel = i.getChannel()
+            channel.Send({"action":"newtext","message":message})
+            
+    def postToAll(self, username, message):
+        for i in users:
+            channel = i.getChannel()
+            channel.Send({"action":"post","username":username,"message":message})
+            
+    def slashHug(self, toUsername, fromUsername):
+        for i in users:
+            if i.getUsername().lower() == toUsername.lower():
+                channel = i.getChannel()
+                channel.Send({"action":"hug","from":fromUsername})
+                
+    def findChannel(self, username):
+        for i in users:
+            if i.getUsername().lower() == username.lower():
+                return i.getChannel()
+                
+    def findUser(self, username):
+        for i in users:
+            if i.getUsername().lower() == username.lower():
+                return i
+                
+    def getUsernames(self):
+        tempUsers = []
+        for i in users:
+            tempUsers.append(i.getUsername())
+        return tempUsers
+                
+    def sendUsernames(self, toUsername):
+        #Grab the channel:
+        channel = self.findChannel(toUsername)
+        usernames = self.getUsernames()
+        channel.Send({"action":"userList","message":usernames})
+        
+    def passwordRequest(self, username, testPassword):
+        if testPassword == password:
+            channel = self.findChannel(username)
+            channel.Send({"action":"adminAccept"})
+            
+    def changeUsername(self, oldUsername, newUsername):
+        user = self.findUser(oldUsername)
+        user.setUsername(newUsername)
+        user.getChannel().Send({"action":"newUsername","username":newUsername})
+        
+    def firstSendUsernames(self):
+        usernames = self.getUsernames()
+        self.tempChannel.Send({"action":"userList","message":usernames})
+                
+                
+address=raw_input("Start the server on what address (192.168.1.144:8000): ")
+if not address:
+    host, port="192.168.1.144", 8000
+else:
+    host, port=address.split(":")
+    
+password = raw_input("Please supply a password for admins : ")
+if not password:
+    password = "AA"
+        
+print "Starting server on host: %s, port:%s" % (host, int(port))
+users = []
+boxesServe=BoxesServer(localaddr=(host, int(port)))
+
+while True:
+    boxesServe.Pump()
+    sleep(0.01)
